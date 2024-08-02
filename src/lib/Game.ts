2c0a14ded8413f5763252d3b071458dd958e0511
cubedhuang <99danielh@gmail.com>
2024-08-02 12:47:07 -0400

    shrink player

--- src/lib/Game.ts
+++ src/lib/Game.ts
@@ -14,7 +14,7 @@ export class Game {
 
 	world = new World();
 	camera = new Camera(0, 0, 800, 600);
-	player = { wx: 3, wy: 10, vx: 0, vy: 0, width: 0.9, height: 1.8 };
+	player = { wx: 3, wy: 10, vx: 0, vy: 0, width: 0.8, height: 1.8 };
 	keys = new Set<string>();
 	renderer: Renderer;
 
